0362a469165880818f326e26a82777cea593c5ae
Rickson <ricksonrocha0@gmail.com>
2023-04-02 21:22:38 -0300

    :sparkles: feat: Adição de filtro para pedidos e totalizadores nos cards

--- src/app/pages/client/client.utils.ts
+++ src/app/pages/client/client.utils.ts
@@ -1,1 +1,4 @@
 export const dateFormatter = (date: Date) => date.toLocaleString()
+
+const currencyBRL = { style: 'currency', currency: 'USD' }
+export const currencyFormatter = (price: number) => price.toLocaleString('pt-BR', currencyBRL)
