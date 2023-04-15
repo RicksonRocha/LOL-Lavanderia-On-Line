aa07674b984b40286ae8e7878091f446db30b01d
Rickson <ricksonrocha0@gmail.com>
2023-04-15 18:29:45 -0300

    :sparkles: feat: Adição de busca de informações do pedido

--- src/app/pages/client/client.component.ts
+++ src/app/pages/client/client.component.ts
@@ -5,34 +5,34 @@ import { IOrders, IStatus, ITableHeads, ORDERS, REPORTS, TABLEHEADS } from './cl
 @Component({
   selector: 'app-client',
   templateUrl: './client.component.html',
-  styleUrls: ['./client.component.scss']
+  styleUrls: ['./client.component.scss'],
 })
-
 export class ClientComponent implements OnInit {
+  public status: IStatus = 'aberto';
+  public orders: IOrders[];
+  public tableHeads: ITableHeads[];
+  public reports: any[];
 
-    public status: IStatus = 'aberto';
-    public orders: IOrders[];
-    public tableHeads: ITableHeads[];
-    public reports: any[];
+  constructor() {}
 
-    constructor() { }
+  ngOnInit() {
+    this.orders = ORDERS.filter((order) => order.status === this.status);
+    this.tableHeads = TABLEHEADS.filter((head) => head);
+    this.reports = REPORTS.map((report) => ({
+      ...report,
+      total: this.lengthByStatus(report.title),
+    }));
+  }
 
-    ngOnInit() {
-        this.orders = ORDERS.filter(order => order.status === this.status);
-        this.tableHeads = TABLEHEADS.filter(head => head);
-        this.reports = REPORTS.map(report => ({...report, total: this.lengthByStatus(report.title)}))
-    }
+  private filterStatus(status: IStatus) {
+    return ORDERS.filter((order) => (status === 'tudo' ? order : order.status === status));
+  }
 
-    private filterStatus(status: IStatus) {
-        return ORDERS.filter(order => status === 'tudo' ? order : order.status === status);
-    }
+  public handleStatus(newStatus: IStatus) {
+    this.orders = this.filterStatus(newStatus);
+  }
 
-    public handleStatus(newStatus: IStatus) {
-        this.orders = this.filterStatus(newStatus)
-    }
-
-    public lengthByStatus(status: IStatus) {
-        return this.filterStatus(status).length
-    }
-
+  public lengthByStatus(status: IStatus) {
+    return this.filterStatus(status).length;
+  }
 }
