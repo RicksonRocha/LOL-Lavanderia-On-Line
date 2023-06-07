9b6dae3808e9e7e6a842b654bdb36d890d82dcd1
PedroHorning <pedro.horning2546@gmail.com>
2023-06-07 18:38:55 -0300

    ajustes layout

--- src/app/pages/relatorios/relatorios.component.ts
+++ src/app/pages/relatorios/relatorios.component.ts
@@ -7,24 +7,7 @@ import axios from 'axios';
   styleUrls: ['./relatorios.component.scss'],
 })
 export class RelatoriosComponent implements OnInit {
-  private endpoint = `https://pdfmyurl.com/api?license=yourlicensekey&url=https://gist.github.com`;
   constructor() {}
 
   ngOnInit() {}
-
-  public async handleDownload() {
-    try {
-      const resp = await axios.get(this.endpoint, {
-        responseType: 'blob',
-      });
-
-      // create temp download url
-      const downloadUrl = window.URL.createObjectURL(resp.data);
-
-      // open pdf file on new tab
-      window.open(downloadUrl, '__blank');
-    } catch (e) {
-      throw e;
-    }
-  }
 }
