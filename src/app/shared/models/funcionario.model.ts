e1e285812128a7ac5d853eabac443b55b29b5add
PedroHorning <pedro.horning2546@gmail.com>
2023-06-23 16:48:21 -0300

    Conectado com api do back de roupas e funcionarios

--- src/app/shared/models/funcionario.model.ts
+++ src/app/shared/models/funcionario.model.ts
@@ -1,9 +1,9 @@
 export class Funcionario {
-    public constructor(
-        public id?: number,
-        public name?: string,
-        public email?: string,
-        public password?: string,
-        public birth_date?: Date
-    ) {}
+  public constructor(
+    public id?: number,
+    public name?: string,
+    public email?: string,
+    public password?: string,
+    public birth_date?: Date
+  ) {}
 }
